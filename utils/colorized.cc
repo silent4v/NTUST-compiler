150279f61ed8421fca4a6dffd3bff0724b6a6dfd
B10715042侯冠年 <B10715042@mail.ntust.edu.tw>
2022-04-04 06:25:40 +0800

    colorized 拆分

---
+++ utils/colorized.cc
@@ -0,0 +1,28 @@
+#include "colorized.hh"
+
+template<typename T>
+std::function<std::string(T&& msg)> colorized(const char* ColorCode)
+{
+  return [ColorCode](T&& message) -> T {
+    std::stringstream ss;
+    ss << ColorCode << message << RESET;
+    return ss.str();
+  };
+}
+
+void reset() {
+  std::cout << RESET;
+}
+
+TPrintFunction red    = colorized<std::string>(RED);
+TPrintFunction green  = colorized<std::string>(GREEN);
+TPrintFunction yellow = colorized<std::string>(YELLOW);
+TPrintFunction blue   = colorized<std::string>(BLUE);
+TPrintFunction purple = colorized<std::string>(PURPLE);
+TPrintFunction cyan   = colorized<std::string>(CYAN);
+
+TPrintFunction id = red;
+TPrintFunction symbol = blue;
+TPrintFunction keyword = green;
+TPrintFunction operators = purple;
+TPrintFunction comment = yellow;
--- utils/colorized.hh
+++ utils/colorized.hh
@@ -1,8 +1,10 @@
 #ifndef _COLORIZED_H_
 #define _COLORIZED_H_
+
 #include <iostream>
 #include <functional>
 #include <sstream>
+#include <string>
 
 #define RED     "\033[0;31m"
 #define GREEN   "\033[0;32m"
@@ -12,30 +14,27 @@
 #define CYAN    "\033[0;36m"
 #define RESET   "\033[0;37m"
 
+
 template<typename T>
-std::function<std::string(T&& msg)> colorized(const char* ColorCode) 
-{
-  return [ColorCode](T&& message) -> T {
-    std::stringstream ss;
-    ss << ColorCode << message << RESET;
-    return ss.str();
-  };
-}
+std::function<std::string(T&& msg)> colorized(const char* ColorCode);
 
-void reset() {
-  std::cout << RESET;
-}
+void reset();
 
-auto red = colorized<std::string>(RED);
-auto green = colorized<std::string>(GREEN);
-auto yellow = colorized<std::string>(YELLOW);
-auto blue = colorized<std::string>(BLUE);
-auto purple = colorized<std::string>(PURPLE);
-auto cyan = colorized<std::string>(CYAN);
+using TPrintFunction = decltype(colorized<std::string>(RED));
 
-auto id = red;
-auto symbol = blue;
-auto keyword = green;
-auto operators = purple;
-auto comment = yellow;
+/* Define Colorful Print function */
+extern TPrintFunction red;
+extern TPrintFunction green;
+extern TPrintFunction yellow;
+extern TPrintFunction blue;
+extern TPrintFunction purple;
+extern TPrintFunction cyan;
+
+/* Special color for token-util */
+extern TPrintFunction id;
+extern TPrintFunction symbol;
+extern TPrintFunction keyword;
+extern TPrintFunction operators;
+extern TPrintFunction comment;
+
 #endif /* _COLORIZED_H_ */
