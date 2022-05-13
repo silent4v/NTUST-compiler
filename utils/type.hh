a0e28f1f90c027a0cd3a8634fa4724332382d067
B10715042侯冠年 <b10715042@mail.ntust.edu.tw>
2022-05-14 00:44:19 +0800

    額外定義 T_ARG 屬性

--- utils/type.hh
+++ utils/type.hh
@@ -17,17 +17,20 @@
 #define T_STRING  0b00001000
 #define T_CONST   0b00010000
 #define T_ARRAY   0b00100000
-#define T_FN      0b01000000
-
+#define T_ARG     0b01000000
+#define T_FN      0b10000000
+using Pair = std::pair<std::string, uint8_t>;
 
 const char* typeinfo(uint8_t typeCode);
-const char* typeinfo(std::pair<std::string, uint8_t> id);
+const char* typeinfo(Pair id);
+void typeCheck(Pair p1, Pair p2);
+void typeCheck(uint8_t t1, uint8_t t2);
 
 struct YYType
 {
   int state;
   uint8_t type;
-  std::pair<std::string, uint8_t> context;
+  Pair context;
 };
 
 
