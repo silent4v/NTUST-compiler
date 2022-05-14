87ee9dd490810803ec18ae9389bffd54c1f15863
B10715042侯冠年 <b10715042@mail.ntust.edu.tw>
2022-05-15 00:09:53 +0800

    SymbolTable 大改版

--- utils/symbolTable.cc
+++ utils/symbolTable.cc
@@ -1,110 +1,82 @@
 #include "symbolTable.hh"
 #include "type.hh"
 
-SymbolTable::SymbolTable() { this->layerIndex_ = 0; }
-void SymbolTable::enableDebug() { this->debug_ = true; }
+std::shared_ptr<SymbolTable> SymbolTable::cursor = nullptr;
+bool SymbolTable::debug_mode = false;
+
+SymbolTable::SymbolTable() {
+  this->parent_ = nullptr;
+  this->children_.reserve(20);
+  this->scopeName = "__GLOBAL__";
+}
+
+void SymbolTable::enableDebug() { SymbolTable::debug_mode = true; }
 
 void SymbolTable::create(std::string className) {
-  this->layerIndex_ = 0;
-  std::pair<std::string, Table> globalScope = {className, {}};
-  this->layer_.push_back(globalScope);
-  if (this->debug_)
-    std::cout << "CLASS " << symbol("[" + className + "]") << "\n";
+  auto globalScope = std::make_shared<SymbolTable>();
+  globalScope->scopeName = className;
+  SymbolTable::cursor = globalScope;
+
+  DEBUG("Application: " + symbol(className))
 }
 
-void SymbolTable::flash() {
-  auto snapshot = this->layer_[this->layerIndex_];
-  std::cout << this->layerIndex_ << ", " << snapshot.second.size() << "\n";
-  this->GLOBAL_.push_back(
-      std::make_pair(this->layerIndex_, this->currentScope_()));
-  this->currentScope_().clear();
-}
-
-void SymbolTable::next(std::string scopeName) {
-  std::pair<std::string, Table> scope = {scopeName, {}};
-  auto from = this->layer_[this->layerIndex_].first;
-  this->flash();
-  this->layer_.push_back(scope);
-  this->layerIndex_++;
-  auto to = this->layer_[this->layerIndex_].first;
-  if (this->debug_)
-    std::cout << "\nScope [" << symbol(from) << " -> " << symbol(to) << "]\n";
+void SymbolTable::enter(std::string scopeName) {
+  auto scope = std::make_shared<SymbolTable>();
+  scope->scopeName = scopeName;
+  scope->parent_ = this->shared_from_this();
+  this->children_.push_back(scope);
+  SymbolTable::cursor = scope;
+  DEBUG(symbol(SymbolTable::cursor->scopeName) + " --> " + symbol(scopeName))
 }
 
 void SymbolTable::exit() {
-  auto from = this->layer_[this->layerIndex_].first;
-  this->flash();
-  this->layer_.pop_back();
-  this->layerIndex_--;
-  auto& to = this->layer_[this->layerIndex_].first;
-  to.clear();
-  if (this->debug_)
-    std::cout << "\nScope [" << symbol(from) << " -> " << symbol(to) << "]\n";
+  auto prevScopeName = SymbolTable::cursor->scopeName;
+  if (SymbolTable::cursor->parent_ != nullptr) {
+    SymbolTable::cursor = SymbolTable::cursor->parent_;
+  }
+  DEBUG(symbol(prevScopeName) + " --> " + symbol(SymbolTable::cursor->scopeName))
 }
 
-void SymbolTable::insert(std::string name, uint8_t type, std::string ctx) {
-  auto &scope = this->currentScope_();
-  if (std::any_of(scope.begin(), scope.end(),
-                  [&name](auto &var) { return NAME(var) == name; })) {
-    std::cout << red("Fatal Error: ") << "`" + name + "`"
-              << " has been declared.\n";
-    std::exit(-1);
+bool SymbolTable::exists(std::string name) {
+  bool find = false;
+  auto cursor = SymbolTable::cursor;
+  while (cursor->parent_ != nullptr) {
+    auto currCheckTable = SymbolTable::cursor->symbols_;
+    auto result = std::any_of(currCheckTable.cbegin(), currCheckTable.cend(),
+                              [&name](auto &var) { return NAME(var) == name; });
+    if (result) {
+      find = true;
+      DEBUG(red(name) + " exists @" + symbol(SymbolTable::cursor->scopeName))
+    }
+    cursor = cursor->parent_;
   }
+}
 
-  auto variable = std::make_tuple(name, type, ctx);
-
-  scope.push_back(variable);
-  if (this->debug_) {
-    if (type & T_FN) {
-      std::cout << "[Function] " << NAME(variable) << ": "
-                << keyword(typeinfo(TYPE(variable))) << "\n";
-    } else if (type & T_ARG) {
-      std::cout << "[define-arg] " << NAME(variable) << "<"
-                << keyword(typeinfo(TYPE(variable))) << ">\n";
-    } else if (type & T_ARRAY) {
-      std::cout << "[insert] " << NAME(variable) << "<"
-                << keyword(typeinfo(TYPE(variable))) << "[]>\n";
+Symbol SymbolTable::lookup(std::string name) {
+  auto cursor = SymbolTable::cursor;
+  while (SymbolTable::cursor->parent_ != nullptr) {
+    auto currCheckTable = SymbolTable::cursor->symbols_;
+    auto result =
+        std::find_if(currCheckTable.cbegin(), currCheckTable.cend(),
+                     [&name](auto &var) { return NAME(var) == name; });
+    if (result != currCheckTable.cend()) {
+      return *result;
     } else {
-      std::cout << "[insert] " << NAME(variable) << "<"
-                << keyword(typeinfo(TYPE(variable))) << ">\n";
+      cursor = cursor->parent_;
     }
   }
+  std::cout << red("Fatal Error:") << symbol(name) << " not found";
+  std::exit(-1);
 }
 
-State SymbolTable::lookup(std::string name) {
-  auto &scope = this->currentScope_();
-  auto iter = std::find_if(scope.cbegin(), scope.cend(),
-                           [&name](auto &var) { return NAME(var) == name; });
-
-  if (iter == scope.end()) {
-    std::cout << red("Fatal Error: ") << "`" + name + "`"
-              << " not found.\n";
+Symbol SymbolTable::insert(std::string name, uint8_t type, std::string ctx) {
+  auto &scope = SymbolTable::cursor;
+  if (scope->exists(name)) {
+    std::cout << red("Fatal Error:") << symbol(name) << " already exists";
     std::exit(-1);
   }
-
-  return *iter;
-}
-
-Table &SymbolTable::currentScope_() {
-  return this->layer_[this->layerIndex_].second;
-}
-
-void SymbolTable::dump() const {
-  auto setIndent = [](int n) {
-    std::string space = "";
-    for (int i = 0; i < n; ++i) {
-      space += "  ";
-    }
-    return space;
-  };
-
-  for (auto scope : this->GLOBAL_) {
-    auto indent = setIndent(scope.first);
-    auto table = scope.second;
-    std::cout << cyan("Layer: ") << "\n";
-    for (auto token : table) {
-      std::cout << indent << NAME(token) << ":" << typeinfo(TYPE(token))
-                << " = " << VALUE(token) << "\n";
-    }
-  }
+  auto tuple = std::make_tuple(name, type, ctx);
+  scope->symbols_.push_back(tuple);
+  DEBUG("[INSERT] " + symbol(name) + "<" + typeinfo(type) + ">")
+  return tuple;
 }
--- utils/symbolTable.hh
+++ utils/symbolTable.hh
@@ -3,39 +3,57 @@
 
 #include "colorized.hh"
 #include "type.hh"
+#include <memory>
 #include <iomanip>
 #include <iostream>
-#include <map>
 #include <utility>
 #include <vector>
 
 #define NAME(key) std::get<0>(key)
 #define TYPE(key) std::get<1>(key)
 #define VALUE(key) std::get<2>(key)
+#define DEBUG(msg) { if(SymbolTable::debug_mode) std::cout << (msg) << std::endl; }
 
+using Symbol = std::tuple<std::string, uint8_t, std::string>;
+using Table = std::vector<Symbol>;
 
-using State = std::tuple<std::string, uint8_t, std::string>;
-using Table = std::vector<State>;
-using Layer = std::pair<std::string, Table>;
-class SymbolTable {
+class SymbolTable: public std::enable_shared_from_this<SymbolTable>
+{
 public:
-  SymbolTable();
-  void enableDebug();
-  void create(std::string className = "_GLOBAL_");
-  void flash();
-  void next(std::string scopeName);
-  void exit();
-  void insert(std::string name, uint8_t type, std::string ctx = "\r");
-  State lookup(std::string name);
-  void insert(const char *name, std::string type);
-  void dump() const;
+  /* Always point to the currently accessed block */
+  static std::shared_ptr<SymbolTable> cursor;
 
+  /* if true, print debug info */
+  static bool debug_mode;
+
+  /* set `debug_mode_` flag true */
+  void enableDebug();
+
+  /* when program state, generate root scope */
+  void create(std::string className = "_CLASS_ENTRY_");
+
+  void enter(std::string scopeName);
+
+  void exit();
+
+  bool exists(std::string name);
+
+  Symbol lookup(std::string name);
+
+  Symbol insert(std::string name, uint8_t type, std::string ctx= "\r\r");
+
+  /* variable store in currently accessed */
+  Table symbols_;
+  
+  /* Current scope name */
+  std::string scopeName;
 private:
-  Table &currentScope_();
-  std::vector<Layer> layer_;
-  std::vector<std::pair<unsigned, Table>> GLOBAL_;
-  int layerIndex_;
-  bool debug_;
+
+  /* point to the parent of currently accessed block */
+  std::shared_ptr<SymbolTable> parent_;
+
+  /* branchs of self node, */
+  std::vector<std::shared_ptr<SymbolTable>> children_;
 };
 
 #endif /* _SYMBOL_TABLE_H_ */
